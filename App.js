3cd9eb9c56746290ab00a37ae6a90367bb008226
Martin Doychev <martin.doychev@abv.bg>
2023-11-12 19:34:56 +0200

    navigation

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { globalStyles } from "./src/globalStyles";
 import { useFonts } from "expo-font";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Login from "./src/screens/Login";
+
+const stackScreenOptions = {
+  headerShown: false,
+  animationEnabled: false,
+};
+const generalScreenOptions = {
+  swipeEnabled: false,
+  headerShown: false,
+  drawerPosition: "right",
+  drawerType: "front",
+};
+
+const Stack = createStackNavigator();
+
+function GetStackNavigator() {
+  return (
+    <Stack.Navigator screenOptions={stackScreenOptions}>
+      <Stack.Screen name="Login" component={Login} />
+      {/* <Stack.Screen name="Login" component={Login} /> */}
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   const [fontsLoaded, fontError] = useFonts({
@@ -16,12 +41,15 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={globalStyles.headline}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={generalScreenOptions}
+        backBehavior="history"
+      >
+        <Stack.Screen name="Euro24Predictor" component={GetStackNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+import { globalStyles } from "../globalStyles";
+
+export default function Login() {
+  return (
+    <SafeAreaView style={globalStyles.container}>
+      <Text>test</Text>
+    </SafeAreaView>
+  );
+}
